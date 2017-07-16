0c23717f6276d2980910934ebe711d8f5ae0367e
Adam Simonini <adam.simonini@gmail.com>
2017-07-15 21:00:33 -0400

    Added Dropdown List Functionality
    Now adds new user-made satelites to array via .push() and also
    dynamically populates the satelite dropdown list.

--- scr.js
+++ scr.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
 
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
+  function Satelite(name, avgAlt, type){
+    this.name = name;
+    this.avgAlt = avgAlt;
+  }
 
+  var sateliteArray = [new Satelite("moon", 3844000), new Satelite("ISS", 400)];
 
   var master = document.getElementById("masterDiv");
 
@@ -12,6 +17,15 @@ $(document).ready(function(){
     master.prepend(newDiv);
   }
 
+  function addSatelite(name, avgAlt){
+    var newSatelite = document.createElement("li");
+    newSatelite.innerHTML = "<a href='#'>"+name+"</a>";
+    dropdown.append(newSatelite);
+  }
+
+  function createSatelite(name, avgAlt){
+    sateliteArray.push(new Satelite(name, avgAlt));
+  }
 
   function clearDiv()
   {
@@ -19,27 +33,30 @@ $(document).ready(function(){
   }
 
   $("#calculateBtn").on("click", function(){
-    var conclusion = document.getElementById("conclusion");
     if(masterDiv.innerHTML !== null){
       clearDiv();
     }
     var name = document.getElementById("name").value;
-    var altitude = document.getElementById("altitude").value;
+    var avgAlt = document.getElementById("avgAlt").value;
 
     if(name === ''){
-      alert("Please input a string");
+      alert("Please name the satelite");
       return;
     }
-    if(altitude === ''){
-      alert("Please enter a number over 1999")
+    if(avgAlt === ''){
+      alert("Please enter a number over 160")
       return;
-    }else if(isNaN(altitude)){
-      alert('"' + altitude + '"' + " is not a number. Please enter a number above 1999")
-    }else if(altitude < 1999){
-      alert("An altitude below 2000km will result in orbital decay. Try a higher number.");
+    }else if(isNaN(avgAlt)){
+      alert('"' + avgAlt + '"' + " is not a number. Please enter a number above 160")
+    }else if(avgAlt < 160){
+      alert("An altitude below 160km will result in sever orbital decay. Try a higher number.");
       return;
+    }else{
+      createSatelite(name, avgAlt);
+      addSatelite(name, avgAlt);
+      console.log(sateliteArray);
     }
-  });
+   });
 
 });
 
